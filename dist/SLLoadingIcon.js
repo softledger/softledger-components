51f4bbcc94d03afce1c1792b09cc6c138823206a
Geoff Ostrega <geoff.ostrega@softledger.com>
2018-03-29 09:40:04 -0700

    removed css import from loading icon

--- dist/SLLoadingIcon.js
+++ dist/SLLoadingIcon.js
@@ -1,6 +1,5 @@
 'use strict';Object.defineProperty(exports, "__esModule", { value: true });
-var _react = require('react');var _react2 = _interopRequireDefault(_react);
-require('./SLLoadingIcon.css');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+var _react = require('react');var _react2 = _interopRequireDefault(_react);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 var SLLoadingIcon = function SLLoadingIcon() {
   var style = {
--- src/SLLoadingIcon.jsx
+++ src/SLLoadingIcon.jsx
@@ -1,6 +1,5 @@
 'use strict';
 import React from 'react';
-import './SLLoadingIcon.css';
 
 const SLLoadingIcon = () => {
 	const style = {
